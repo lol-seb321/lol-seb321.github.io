f47f5556b8f459fb3ad7c8645e3c3cdcb0e525ca
Sebastian Ferreira <sjferreira.99@gmail.com>
2023-12-12 19:19:08 -0500

    URLsAdded

--- js/main.js
+++ js/main.js
@@ -5,5 +5,24 @@ window.addEventListener('mousemove', function(e) {
   customCursor.style.top = e.clientY + 'px';
   customCursor.style.display = 'block';
 });
-
 </script>
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Resume, Email, LinkedIn are IDs given
+    const resumeButton = document.querySelector('.neon-button#Resume');
+    const emailButton = document.querySelector('.neon-button#Email');
+    const linkedInButton = document.querySelector('.neon-button#LinkedIn');
+
+    resumeButton.addEventListener('click', () => {
+        window.location.href = 'https://';
+    });
+
+    emailButton.addEventListener('click', () => {
+        window.location.href = 'https://';
+    });
+
+
+    linkedInButton.addEventListener('click', () => {
+        window.location.href = 'https://www.linkedin.com/in/sebastian-ferreira-570961193/';
+    });
+});
